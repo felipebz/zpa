3aa44c5bba813e1f5612ee9896f7b203005c8568
Felipe Zorzo <felipe.zorzo@maxiconsystems.com.br>
2015-11-09 23:45:38 -0200

    Fix highlighter tests in Windows and check cr/lf combinations

--- sonar-plsql-open-plugin/src/test/java/org/sonar/plsqlopen/highlight/PlSqlHighlighterTest.java
+++ sonar-plsql-open-plugin/src/test/java/org/sonar/plsqlopen/highlight/PlSqlHighlighterTest.java
@@ -25,49 +25,91 @@ import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import org.mockito.Mockito;
 import org.sonar.api.batch.fs.InputFile;
-import org.sonar.api.batch.fs.internal.DefaultFileSystem;
-import org.sonar.api.batch.fs.internal.DefaultInputFile;
 import org.sonar.api.component.ResourcePerspectives;
 import org.sonar.api.source.Highlightable;
-import org.sonar.plsqlopen.PlSql;
 import org.sonar.plsqlopen.squid.PlSqlConfiguration;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
 
 public class PlSqlHighlighterTest {
 
+    @Rule
+    public TemporaryFolder temp = new TemporaryFolder();
+  
     private PlSqlHighlighter highlighter;
-    private DefaultFileSystem fs = new DefaultFileSystem(new File("."));
-    ResourcePerspectives perspectives;
+    private ResourcePerspectives perspectives;
+    private List<String> lines;
+    private String eol;
+    private Highlightable highlightable;
+    private Highlightable.HighlightingBuilder highlightingBuilder;
 
     @Before
     public void setUp() {
         perspectives = mock(ResourcePerspectives.class);
-        highlighter = new PlSqlHighlighter(new PlSqlConfiguration(fs.encoding()));
+        highlighter = new PlSqlHighlighter(new PlSqlConfiguration(Charsets.UTF_8));
+        highlightable = mock(Highlightable.class);
+        highlightingBuilder = mock(Highlightable.HighlightingBuilder.class);
+        when(perspectives.as(Mockito.eq(Highlightable.class), Mockito.any(InputFile.class))).thenReturn(highlightable);
+        when(highlightable.newHighlighting()).thenReturn(highlightingBuilder);
+    }
+    
+    private File generateTestFile() throws IOException {
+        File file = temp.newFile();
+        String content = Files.toString(new File("src/test/resources/br/com/felipezorzo/sonar/plsql/highlight.sql"), Charsets.UTF_8);
+        Files.write(content.replaceAll("\\r\\n", "\n").replaceAll("\\n", eol), file, Charsets.UTF_8);
+        return file;
+    }
+    
+    private int offset(int line, int column) {
+    	int result = 0;
+    	for (int i = 0; i < line - 1; i++) {
+    		result += lines.get(i).length() + eol.length();
+    	}
+    	result += column - 1;
+    	return result;
     }
 
     @Test
-    public void shouldAnalyse() {
-        String relativePath = "src/test/resources/br/com/felipezorzo/sonar/plsql/highlight.sql";
-        DefaultInputFile inputFile = new DefaultInputFile(relativePath).setLanguage(PlSql.KEY);
-        inputFile.setAbsolutePath((new File(relativePath)).getAbsolutePath());
-        fs.add(inputFile);
+    public void shouldAnalyse_lf() throws IOException {
+    	eol = "\n";
+    	File file = generateTestFile();
+    	verifyHighlighting(file);
+    }
+    
+    @Test
+    public void shouldAnalyse_crlf() throws IOException {
+    	eol = "\r\n";
+    	File file = generateTestFile();
+    	verifyHighlighting(file);
+    }
+    
+    @Test
+    public void shouldAnalyse_cr() throws IOException {
+    	eol = "\r";
+    	File file = generateTestFile();
+    	verifyHighlighting(file);
+    }
+    
+    private void verifyHighlighting(File file) throws IOException {
+    	lines = Files.readLines(file, Charsets.UTF_8);
 
-        Highlightable highlightable = mock(Highlightable.class);
-        Highlightable.HighlightingBuilder highlightingBuilder = mock(Highlightable.HighlightingBuilder.class);
-        when(perspectives.as(Mockito.eq(Highlightable.class), Mockito.any(InputFile.class))).thenReturn(highlightable);
-        when(highlightable.newHighlighting()).thenReturn(highlightingBuilder);
+        highlighter.highlight(highlightable, file);
 
-        highlighter.highlight(highlightable, inputFile.file());
-
-        verify(highlightingBuilder).highlight(0, 5, "k");
-        verify(highlightingBuilder).highlight(8, 28, "cd");
-        verify(highlightingBuilder).highlight(31, 61, "j");
-        verify(highlightingBuilder).highlight(69, 70, "s");
-        verify(highlightingBuilder).highlight(72, 75, "k");
+        verify(highlightingBuilder).highlight(offset(1, 1), offset(1, 6), "k");
+        verify(highlightingBuilder).highlight(offset(2, 3), offset(2, 23), "cd");
+        verify(highlightingBuilder).highlight(offset(3, 3), offset(5, 6), "j");
+        verify(highlightingBuilder).highlight(offset(6, 8), offset(6, 9), "s");
+        verify(highlightingBuilder).highlight(offset(7, 1), offset(7, 4), "k");
         verify(highlightingBuilder).done();
         verifyNoMoreInteractions(highlightingBuilder);
     }
