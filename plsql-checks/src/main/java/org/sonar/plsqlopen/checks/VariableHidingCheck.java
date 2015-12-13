e378a72e07416ac9b7910423a9aaf88cfcb22732
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2015-12-13 21:34:07 -0200

    Refactor VariableHidingCheck to use the symbol table

--- plsql-checks/src/main/java/org/sonar/plsqlopen/checks/VariableHidingCheck.java
+++ plsql-checks/src/main/java/org/sonar/plsqlopen/checks/VariableHidingCheck.java
@@ -19,25 +19,20 @@
  */
 package org.sonar.plsqlopen.checks;
 
-import java.util.ArrayDeque;
 import java.util.Deque;
-
-import javax.annotation.Nullable;
 
 import org.sonar.api.server.rule.RulesDefinition;
 import org.sonar.check.Priority;
 import org.sonar.check.Rule;
-import org.sonar.plsqlopen.checks.scope.Scope;
-import org.sonar.plsqlopen.checks.scope.Variable;
 import org.sonar.plugins.plsqlopen.api.PlSqlGrammar;
-import org.sonar.plugins.plsqlopen.api.PlSqlKeyword;
+import org.sonar.plugins.plsqlopen.api.symbols.Scope;
+import org.sonar.plugins.plsqlopen.api.symbols.Symbol;
 import org.sonar.squidbridge.annotations.ActivatedByDefault;
 import org.sonar.squidbridge.annotations.SqaleConstantRemediation;
 import org.sonar.squidbridge.annotations.SqaleSubCharacteristic;
 
 import com.google.common.collect.ImmutableList;
 import com.sonar.sslr.api.AstNode;
-import com.sonar.sslr.api.AstNodeType;
 
 @Rule(
     key = VariableHidingCheck.CHECK_KEY,
@@ -48,67 +43,32 @@ import com.sonar.sslr.api.AstNodeType;
 @ActivatedByDefault 
 public class VariableHidingCheck extends AbstractBaseCheck {
     public static final String CHECK_KEY = "VariableHiding";
-    private static final AstNodeType[] scopeHolders = { 
-            PlSqlGrammar.ANONYMOUS_BLOCK,
-            PlSqlGrammar.CREATE_PROCEDURE,
-            PlSqlGrammar.PROCEDURE_DECLARATION,
-            PlSqlGrammar.CREATE_FUNCTION,
-            PlSqlGrammar.FUNCTION_DECLARATION,
-            PlSqlGrammar.CREATE_PACKAGE_BODY,
-            PlSqlGrammar.BLOCK_STATEMENT,
-            PlSqlGrammar.FOR_STATEMENT
-            };
-
-    private Deque<Scope> scopes = new ArrayDeque<>();
 
     @Override
     public void init() {
-        subscribeTo(scopeHolders);
         subscribeTo(PlSqlGrammar.VARIABLE_DECLARATION);
     }
-
+    
     @Override
     public void visitNode(AstNode node) {
-        if (node.is(scopeHolders)) {
+        AstNode identifier = node.getFirstChild(PlSqlGrammar.IDENTIFIER_NAME);
+        String name = identifier.getTokenOriginalValue();
+        
+        Scope scope = getPlSqlContext().getSymbolTable().getScopeForSymbol(identifier);
+        if (scope != null) {
+            Deque<Symbol> symbols = scope.getSymbolsAcessibleInScope(name);
             
-            scopes.push(new Scope(getCurrentScope()));
-            
-            if (node.is(PlSqlGrammar.FOR_STATEMENT)) {
-                AstNode identifier = node.getFirstChild(PlSqlKeyword.FOR).getNextSibling();
-                getCurrentScope().declareLocalVariable(identifier, 1);
+            if (symbols.size() > 1) {
+                AstNode originalVariable = symbols.getLast().declaration();
+                
+                if (!originalVariable.equals(identifier)) {
+                    getPlSqlContext().createViolation(this, getLocalizedMessage(CHECK_KEY),
+                            identifier,
+                            ImmutableList.of(newLocation("Original", originalVariable)),
+                            name,
+                            originalVariable.getTokenLine());
+                }
             }
-            
-        } else if (!scopes.isEmpty() && node.is(PlSqlGrammar.VARIABLE_DECLARATION)) {
-            AstNode identifier = node.getFirstChild(PlSqlGrammar.IDENTIFIER_NAME);
-            
-            Variable variable = getCurrentScope().getVariableDeclaration(identifier);
-            if (variable != null) {
-                AstNode originalVariable = variable.getDeclaration();
-                getPlSqlContext().createViolation(this, getLocalizedMessage(CHECK_KEY),
-                        identifier,
-                        ImmutableList.of(newLocation("Original", originalVariable)),
-                        identifier.getTokenOriginalValue(),
-                        originalVariable.getTokenLine());
-            }
-            
-            getCurrentScope().declareLocalVariable(identifier);
         }
     }
-    
-    @Override
-    public void leaveNode(AstNode astNode) {
-      if (astNode.is(scopeHolders)) {
-        scopes.pop();
-      }
-    }
-    
-    @Override
-    public void leaveFile(@Nullable AstNode astNode) {
-        scopes.clear();
-    }
-
-    private Scope getCurrentScope() {
-        return scopes.peek();
-    }
-
 }
