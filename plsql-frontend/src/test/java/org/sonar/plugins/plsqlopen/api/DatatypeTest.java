8dc6da5d1c264641dbdeb7c666413a4e81f89f36
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2016-09-18 18:18:47 -0300

    Add tests for basic datatypes

---
+++ plsql-frontend/src/test/java/org/sonar/plugins/plsqlopen/api/DatatypeTest.java
@@ -0,0 +1,50 @@
+/*
+ * Sonar PL/SQL Plugin (Community)
+ * Copyright (C) 2015-2016 Felipe Zorzo
+ * mailto:felipebzorzo AT gmail DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.plugins.plsqlopen.api;
+
+import static org.sonar.sslr.tests.Assertions.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DatatypeTest extends RuleTest {
+    
+    @Before
+    public void init() {
+        setRootRule(PlSqlGrammar.DATATYPE);
+    }
+
+    @Test
+    public void test() {
+        assertThat(p).matches("number");
+        assertThat(p).matches("number(10)");
+        assertThat(p).matches("number(10, 2)");
+        assertThat(p).matches("clob character set any_cs");
+        assertThat(p).matches("clob character set str%charset");
+        assertThat(p).matches("varchar2(10)");
+        assertThat(p).matches("varchar2(10) character set any_cs");
+        assertThat(p).matches("varchar2(10) character set str%charset");
+        assertThat(p).matches("date");
+        assertThat(p).matches("timestamp");
+        assertThat(p).matches("timestamp with time zone");
+        assertThat(p).matches("timestamp with local time zone");
+    }
+    
+}
