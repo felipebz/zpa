ce855b73ace541d752d77fda1e485ef04068237d
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2017-06-04 23:18:01 -0300

    Fix custom rules example

--- plsql-custom-rules/src/test/java/com/company/plsql/PlSqlCustomRulesPluginTest.java
+++ plsql-custom-rules/src/test/java/com/company/plsql/PlSqlCustomRulesPluginTest.java
@@ -4,7 +4,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Test;
 import org.sonar.api.Plugin;
-import org.sonar.api.SonarQubeVersion;
+import org.sonar.api.SonarQubeSide;
+import org.sonar.api.internal.SonarRuntimeImpl;
+import org.sonar.api.utils.Version;
 
 import com.company.plsql.PlSqlCustomRulesPlugin;
 
@@ -12,7 +14,7 @@ public class PlSqlCustomRulesPluginTest {
 
     @Test
     public void test() {
-        Plugin.Context context = new Plugin.Context(SonarQubeVersion.V5_6);
+        Plugin.Context context = new Plugin.Context(SonarRuntimeImpl.forSonarQube(Version.create(6, 0), SonarQubeSide.SERVER));
         PlSqlCustomRulesPlugin plugin = new PlSqlCustomRulesPlugin();
         plugin.define(context);
         assertThat(context.getExtensions().size()).isEqualTo(1);
