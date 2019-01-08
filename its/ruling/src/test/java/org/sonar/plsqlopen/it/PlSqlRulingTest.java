42630cf351d99bae9f42b516d7c5c383852494aa
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2019-01-08 21:10:59 -0200

    Speed up integration tests

--- its/ruling/src/test/java/org/sonar/plsqlopen/it/PlSqlRulingTest.java
+++ its/ruling/src/test/java/org/sonar/plsqlopen/it/PlSqlRulingTest.java
@@ -50,26 +50,13 @@ public class PlSqlRulingTest {
       .build();
 
     public SonarScanner initializeBuild(String projectId) {
-        return initializeBuild(projectId, null, "rules");
+        return initializeBuild(projectId, "rules");
     }
-
-    public SonarScanner initializeFormsBuild(String projectId, String sources) {
-        SonarScanner build = initializeBuild(projectId, sources, "forms_rules");
-        build.setProperty("sonar.plsql.forms.metadata", "metadata.json");
-        build.setProperty("sonar.plsql.file.suffixes", "pcd,fun,pks,pkb,tgg");
-        return build;
-    }
-
-    public SonarScanner initializeBuild(String projectId, String sources, String profileName) {
+    public SonarScanner initializeBuild(String projectId, String profileName) {
         orchestrator.getServer().provisionProject(projectId, projectId);
         orchestrator.getServer().associateProjectToQualityProfile(projectId, "plsqlopen", profileName);
 
-        String originalSource = sources;
-        if (originalSource == null) {
-            originalSource = projectId;
-        }
-
-        return SonarScanner.create(FileLocation.of("../sources/" + originalSource).getFile())
+        return SonarScanner.create(FileLocation.of("../sources/" + projectId).getFile())
             .setProjectKey(projectId)
             .setProjectVersion("1")
             .setLanguage("plsqlopen")
@@ -77,7 +64,7 @@ public class PlSqlRulingTest {
             .setSourceDirs(".")
             .setProperty("dump.old", FileLocation.of("src/test/resources/expected/" + projectId).getFile().getAbsolutePath())
             .setProperty("dump.new", FileLocation.of("target/actual/" + projectId).getFile().getAbsolutePath())
-            .setProperty("sonar.plsql.file.suffixes", "sql,typ,pkg,pkb,pks,tab,tps,tpb")
+            .setProperty("sonar.plsql.file.suffixes", "sql,typ,pkg,pkb,pks,tab,tps,tpb,pcd,fun,tgg")
             .setProperty("sonar.plsql.errorRecoveryEnabled", "false")
             .setProperty("sonar.cpd.skip", "true")
             .setProperty("sonar.analysis.mode", "preview")
@@ -124,123 +111,26 @@ public class PlSqlRulingTest {
     }
 
     @Test
-    public void demo0001() throws Exception {
-        SonarScanner build = initializeFormsBuild("demo0001", "Doag-Forms-extracted/demo0001/WEBUTIL_DEMO");
+    public void forms() throws Exception {
+        String[] modules = { "demo0001", "demo0002", "demo0002_2", "demo0003", "demo0004",
+            "demo0005", "demo0006", "demo0007", "demo0008", "demo0009",
+            "demo0010", "demo0011", "demo0012", "demo0013", "demo0014",
+            "demo0014_2", "demo0015", "demo0016", "demo0017", "demo0018" };
+        SonarScanner build = initializeBuild("Doag-Forms-extracted", "forms_rules");
+        build.setSourceDirs("");
+        for (String module : modules) {
+            build.setProperty(module + ".dump.old", FileLocation.of("src/test/resources/expected/" + module).getFile().getAbsolutePath());
+            build.setProperty(module + ".dump.new", FileLocation.of("target/actual/" + module).getFile().getAbsolutePath());
+            build.setProperty(module + ".lits.differences", FileLocation.of("target/differences_" + module).getFile().getAbsolutePath());
+        }
         executeBuild(build);
-    }
 
-    @Test
-    public void demo0002() throws Exception {
-        SonarScanner build = initializeFormsBuild("demo0002", "Doag-Forms-extracted/demo0002/DEMO0002");
-        executeBuild(build);
-    }
+        orchestrator.executeBuild(build);
 
-    @Test
-    public void demo0002_2() throws Exception {
-        SonarScanner build = initializeFormsBuild("demo0002_2", "Doag-Forms-extracted/demo0002/FRW_REF");
-        executeBuild(build);
-    }
-
-    @Test
-    public void demo0003() throws Exception {
-        SonarScanner build = initializeFormsBuild("demo0003", "Doag-Forms-extracted/demo0003/DEMO0003");
-        executeBuild(build);
-    }
-
-    @Test
-    public void demo0004() throws Exception {
-        SonarScanner build = initializeFormsBuild("demo0004", "Doag-Forms-extracted/demo0004/RELEASE_LOCKS");
-        executeBuild(build);
-    }
-
-    @Test
-    public void demo0005() throws Exception {
-        SonarScanner build = initializeFormsBuild("demo0005", "Doag-Forms-extracted/demo0005/TIMEOUTPJC_TEST");
-        executeBuild(build);
-    }
-
-    @Test
-    public void demo0006() throws Exception {
-        SonarScanner build = initializeFormsBuild("demo0006", "Doag-Forms-extracted/demo0006/TIMEOUT_SYS_CLIENT_IDL");
-        executeBuild(build);
-    }
-
-    @Test
-    public void demo0007() throws Exception {
-        SonarScanner build = initializeFormsBuild("demo0007", "Doag-Forms-extracted/demo0007/CD_DEMO_EXCEL");
-        executeBuild(build);
-    }
-
-    @Test
-    public void demo0008() throws Exception {
-        SonarScanner build = initializeFormsBuild("demo0008", "Doag-Forms-extracted/demo0008/LATENCY_TEST");
-        executeBuild(build);
-    }
-
-    @Test
-    public void demo0009() throws Exception {
-        SonarScanner build = initializeFormsBuild("demo0009", "Doag-Forms-extracted/demo0009/FORMSAPI_WIZARD_2905");
-        executeBuild(build);
-    }
-
-    @Test
-    public void demo0010() throws Exception {
-        SonarScanner build = initializeFormsBuild("demo0010", "Doag-Forms-extracted/demo0010/CHK_MYFFI_SAMPLE5");
-        executeBuild(build);
-    }
-
-    @Test
-    public void demo0011() throws Exception {
-        SonarScanner build = initializeFormsBuild("demo0011", "Doag-Forms-extracted/demo0011/WEBUTIL_DEMO");
-        executeBuild(build);
-    }
-
-    @Test
-    public void demo0012() throws Exception {
-        SonarScanner build = initializeFormsBuild("demo0012", "Doag-Forms-extracted/demo0012/PDFVIEWER");
-        executeBuild(build);
-    }
-
-    @Test
-    public void demo0013() throws Exception {
-        SonarScanner build = initializeFormsBuild("demo0013", "Doag-Forms-extracted/demo0013/COLOR_SLIDER");
-        executeBuild(build);
-    }
-
-    @Test
-    public void demo0014() throws Exception {
-        SonarScanner build = initializeFormsBuild("demo0014", "Doag-Forms-extracted/demo0014/ACCORDION");
-        executeBuild(build);
-    }
-
-    @Test
-    public void demo0014_2() throws Exception {
-        SonarScanner build = initializeFormsBuild("demo0014_2", "Doag-Forms-extracted/demo0014/ACCORDION2");
-        executeBuild(build);
-    }
-
-    @Test
-    public void demo0015() throws Exception {
-        SonarScanner build = initializeFormsBuild("demo0015", "Doag-Forms-extracted/demo0015/MODERNIZE");
-        executeBuild(build);
-    }
-
-    @Test
-    public void demo0016() throws Exception {
-        SonarScanner build = initializeFormsBuild("demo0016", "Doag-Forms-extracted/demo0016/CHK_CBOX3");
-        executeBuild(build);
-    }
-
-    @Test
-    public void demo0017() throws Exception {
-        SonarScanner build = initializeFormsBuild("demo0017", "Doag-Forms-extracted/demo0017/POC_ACCOUNT");
-        executeBuild(build);
-    }
-
-    @Test
-    public void demo0018() throws Exception {
-        SonarScanner build = initializeFormsBuild("demo0018", "Doag-Forms-extracted/demo0018/TEST");
-        executeBuild(build);
+        for (String module : modules) {
+            String differences = new String(Files.readAllBytes(Paths.get(FileLocation.of("target/differences_" + module).getFile().getAbsolutePath())), StandardCharsets.UTF_8);
+            assertThat(differences).isEmpty();
+        }
     }
 
 }
