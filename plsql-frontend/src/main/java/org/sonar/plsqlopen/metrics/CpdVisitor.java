ef4094a4f0ca68cae90df26ad53038fbb2c8d63f
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2019-02-05 22:07:48 -0200

    Generate CPD data for type bodies

--- plsql-frontend/src/main/java/org/sonar/plsqlopen/metrics/CpdVisitor.java
+++ plsql-frontend/src/main/java/org/sonar/plsqlopen/metrics/CpdVisitor.java
@@ -25,38 +25,40 @@ import org.sonar.api.batch.sensor.cpd.NewCpdTokens;
 import org.sonar.plsqlopen.TokenLocation;
 import org.sonar.plsqlopen.checks.PlSqlCheck;
 import org.sonar.plugins.plsqlopen.api.PlSqlGrammar;
+import org.sonar.plugins.plsqlopen.api.PlSqlPunctuator;
 
 import com.sonar.sslr.api.AstNode;
 import com.sonar.sslr.api.Token;
 
 public class CpdVisitor extends PlSqlCheck {
-    
+
     private NewCpdTokens newCpdTokens;
-    
+
     public CpdVisitor(SensorContext context, InputFile inputFile) {
         newCpdTokens = context.newCpdTokens().onFile(inputFile);
     }
-    
+
     @Override
     public void init() {
         subscribeTo(PlSqlGrammar.ANONYMOUS_BLOCK,
-                    PlSqlGrammar.CREATE_PROCEDURE, 
-                    PlSqlGrammar.CREATE_FUNCTION,
-                    PlSqlGrammar.CREATE_PACKAGE_BODY);
+            PlSqlGrammar.CREATE_PROCEDURE,
+            PlSqlGrammar.CREATE_FUNCTION,
+            PlSqlGrammar.CREATE_PACKAGE_BODY,
+            PlSqlGrammar.CREATE_TYPE_BODY);
     }
-    
+
     @Override
     public void visitNode(AstNode node) {
         for (Token token : node.getTokens()) {
             saveCpdTokens(token);
         }
     }
-    
+
     public void saveCpdTokens(Token token) {
         TokenLocation location = TokenLocation.from(token);
         newCpdTokens.addToken(location.line(), location.column(), location.endLine(), location.endColumn(), token.getValue());
     }
-    
+
     @Override
     public void leaveFile(AstNode node) {
         newCpdTokens.save();
