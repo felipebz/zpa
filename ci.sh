a17bc12edd154accf75f899355d4da00986619d3
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2019-04-17 21:59:14 -0300

    Switch to JDK 11
    JDK 8 still can be used to build the project but JDK 11 will be used
    by default in the CI pipeline. The only exception is the integration
    tests with SonarQube 6.7.x (LTS), they'll continue to run using JDK 8.
    
    Note that this doesn't require you to run SonarQube using JDK 11. If you
    build this project using JDK 11, it'll run correctly in a SonarQube
    running on JDK 8, just as it always did.

--- ci.sh
+++ ci.sh
@@ -2,6 +2,9 @@
 
 set -euo pipefail
 
+# Set JDK 11 as default
+JAVA_HOME=$JAVA_HOME_11_X64
+
 case "$TESTS" in
 
 ci)
@@ -22,6 +25,11 @@ it)
   ./mvnw -f plsql-custom-rules/pom.xml package -e -B
 
   cd its
+
+  if [ "$SQ_VERSION" == "LATEST_RELEASE[6.7]" ]; then
+    # To run the integration tests with SQ 6.7 we'll to use JDK 8
+    JAVA_HOME=$JAVA_HOME_8_X64
+  fi
   ../mvnw -Dsonar.runtimeVersion="$SQ_VERSION" -Pit verify -e -B -V
   ;;
 
