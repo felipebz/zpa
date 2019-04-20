da550cda4d8f02761899d8cd5a84db92d85a7c83
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2019-04-20 13:17:13 -0300

    Pass the correct branch name to coveralls.io

--- ci.sh
+++ ci.sh
@@ -9,7 +9,7 @@ case "$TESTS" in
 
 ci)
   pre_args="jacoco:prepare-agent"
-  extra_args="jacoco:report org.eluder.coveralls:coveralls-maven-plugin:report -DrepoToken=$COVERALLS_TOKEN"
+  extra_args="jacoco:report org.eluder.coveralls:coveralls-maven-plugin:report -DrepoToken=$COVERALLS_TOKEN -DserviceName=azure-devops -DserviceJobId=$BUILD_BUILDID -Dbranch=$BUILD_SOURCEBRANCHNAME"
   if [ "$BUILD_SOURCEBRANCH" == "refs/heads/master" ] && [ "$SYSTEM_PULLREQUEST_ISFORK" == "False" ]; then
     ./mvnw $pre_args source:jar javadoc:jar deploy $extra_args --settings=.settings.xml -B -e -V
   else
