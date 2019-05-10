f3c9760281ad74699a7529b7c84387a577b88a49
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2019-05-09 22:31:56 -0300

    Add zpa-core module (part of #112)
    The code from plsql-frontend module that doesn't require any SonarQube API will be moved to this new module.
    
    This is the fist module written in Kotlin. Eventually other modules will be also migrated from Java to Kotlin.

---
+++ zpa-core/src/main/kotlin/org/sonar/plsqlopen/TokenLocation.kt
@@ -0,0 +1,64 @@
+/**
+ * Z PL/SQL Analyzer
+ * Copyright (C) 2015-2019 Felipe Zorzo
+ * mailto:felipebzorzo AT gmail DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.plsqlopen
+
+import com.sonar.sslr.api.Token
+
+class TokenLocation private constructor(
+    private val line: Int,
+    private val column: Int,
+    private val endLine: Int,
+    private val endColumn: Int
+) {
+
+    fun line() = line
+
+    fun column() = column
+
+    fun endLine() = endLine
+
+    fun endColumn() = endColumn
+
+    companion object {
+        private val pattern = Regex("\r?\n|\r")
+
+        @JvmStatic
+        fun from(token: Token): TokenLocation {
+            val lineCount: Int
+            var lastLineLength = 0
+
+            if (token.value.contains("\n") || token.value.contains("\r")) {
+                val lines = pattern.split(token.value)
+                lineCount = lines.size
+                lastLineLength = lines[lines.size - 1].length
+            } else {
+                lineCount = 1
+            }
+
+            var endLineOffset = token.column + token.value.length
+            val endLine = token.line + lineCount - 1
+            if (endLine != token.line) {
+                endLineOffset = lastLineLength
+            }
+            return TokenLocation(token.line, token.column, endLine, endLineOffset)
+        }
+    }
+
+}
---
+++ zpa-core/src/main/kotlin/org/sonar/plsqlopen/checks/IssueLocation.kt
@@ -0,0 +1,109 @@
+/**
+ * Z PL/SQL Analyzer
+ * Copyright (C) 2015-2019 Felipe Zorzo
+ * mailto:felipebzorzo AT gmail DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.plsqlopen.checks
+
+import com.sonar.sslr.api.AstNode
+import com.sonar.sslr.api.Token
+import org.sonar.plsqlopen.TokenLocation
+
+abstract class IssueLocation private constructor(private val message: String) {
+
+    fun message() = message
+
+    abstract fun startLine(): Int
+
+    abstract fun startLineOffset(): Int
+
+    abstract fun endLine(): Int
+
+    abstract fun endLineOffset(): Int
+
+    private class PreciseIssueLocation : IssueLocation {
+
+        private val firstToken: Token
+        private val lastTokenLocation: TokenLocation
+
+        constructor(node: AstNode, message: String) : super(message) {
+            this.firstToken = node.token
+            this.lastTokenLocation = TokenLocation.from(node.lastToken)
+        }
+
+        constructor(startNode: AstNode, endNode: AstNode, message: String) : super(message) {
+            this.firstToken = startNode.token
+            this.lastTokenLocation = TokenLocation.from(endNode.lastToken)
+        }
+
+        override fun startLine() = firstToken.line
+
+        override fun startLineOffset() = firstToken.column
+
+        override fun endLine() = lastTokenLocation.endLine()
+
+        override fun endLineOffset() = lastTokenLocation.endColumn()
+
+    }
+
+    private class LineLevelIssueLocation(message: String, private val lineNumber: Int) : IssueLocation(message) {
+
+        override fun startLine() = lineNumber
+
+        override fun startLineOffset() = UNDEFINED_OFFSET
+
+        override fun endLine() = lineNumber
+
+        override fun endLineOffset() = UNDEFINED_OFFSET
+
+    }
+
+    private class FileLevelIssueLocation(message: String) : IssueLocation(message) {
+
+        override fun startLine() = UNDEFINED_LINE
+
+        override fun startLineOffset() = UNDEFINED_OFFSET
+
+        override fun endLine() = UNDEFINED_LINE
+
+        override fun endLineOffset() = UNDEFINED_OFFSET
+
+    }
+
+    companion object {
+
+        const val UNDEFINED_OFFSET = -1
+
+        const val UNDEFINED_LINE = 0
+
+        @JvmStatic
+        fun atFileLevel(message: String): IssueLocation =
+            FileLevelIssueLocation(message)
+
+        @JvmStatic
+        fun atLineLevel(message: String, lineNumber: Int): IssueLocation =
+            LineLevelIssueLocation(message, lineNumber)
+
+        @JvmStatic
+        fun preciseLocation(startNode: AstNode, endNode: AstNode, message: String): IssueLocation =
+            PreciseIssueLocation(startNode, endNode, message)
+
+        @JvmStatic
+        fun preciseLocation(startNode: AstNode, message: String): IssueLocation =
+            PreciseIssueLocation(startNode, message)
+    }
+}
