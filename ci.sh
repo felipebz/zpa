ab410dacd2468a46dbd474ace2ff4049de1bee7a
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2019-05-16 22:35:15 -0300

    Remove Coveralls config

--- ci.sh
+++ ci.sh
@@ -8,12 +8,10 @@ JAVA_HOME=$JAVA_HOME_11_X64
 case "$TESTS" in
 
 ci)
-  pre_args="jacoco:prepare-agent"
-  extra_args="jacoco:report org.eluder.coveralls:coveralls-maven-plugin:report -DrepoToken=$COVERALLS_TOKEN -DserviceName=azure-devops -DserviceJobId=$BUILD_BUILDID -Dbranch=$BUILD_SOURCEBRANCHNAME"
   if [ "$BUILD_SOURCEBRANCH" == "refs/heads/master" ] && [ "$SYSTEM_PULLREQUEST_ISFORK" == "False" ]; then
-    ./mvnw $pre_args source:jar javadoc:jar deploy $extra_args --settings=.settings.xml -B -e -V
+    ./mvnw source:jar javadoc:jar deploy --settings=.settings.xml -B -e -V
   else
-    ./mvnw $pre_args verify $extra_args -B -e -V
+    ./mvnw verify -B -e -V
   fi
   ;;
 
