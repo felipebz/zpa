6e49a544c1b7a2f45b787333a5bb94151a023fda
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2021-01-23 17:52:07 -0300

    Clear sonar.branch.name due a bug in the SonarQubePrepare task
    See: https://github.com/SonarSource/sonar-scanner-vsts/blob/aade3419912669945aabaa989d757da43af37c82/common/ts/prepare-task.ts#L99-L100

--- plsql-custom-rules/src/test/java/com/company/plsql/ForbiddenDmlCheckTest.java
+++ plsql-custom-rules/src/test/java/com/company/plsql/ForbiddenDmlCheckTest.java
@@ -1,13 +1,33 @@
 package com.company.plsql;
 
+import com.sonar.sslr.impl.Parser;
 import org.junit.Test;
 import org.sonar.plsqlopen.checks.verifier.PlSqlCheckVerifier;
+import org.sonar.plsqlopen.parser.PlSqlParser;
+import org.sonar.plsqlopen.squid.PlSqlConfiguration;
+import org.sonar.plugins.plsqlopen.api.PlSqlVisitorContext;
+import org.sonar.plugins.plsqlopen.api.checks.PlSqlCheck;
+import org.sonar.plugins.plsqlopen.api.sslr.AstNode;
+import org.sonar.plugins.plsqlopen.api.sslr.Grammar;
+
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 public class ForbiddenDmlCheckTest {
 
-    @Test
-    public void test() {
-        PlSqlCheckVerifier.verify("src/test/resources/forbidden-dml.sql", new ForbiddenDmlCheck());
-    }
+@Test
+public void test() {
+    File file = new File("src/test/resources/forbidden-dml.sql");
+
+    Parser<Grammar> parser = PlSqlParser.INSTANCE.create(
+        new PlSqlConfiguration(StandardCharsets.UTF_8, true));
+
+    AstNode rootTree = parser.parse(file);
+    PlSqlVisitorContext context = new PlSqlVisitorContext(rootTree, null, null);
+
+    ForbiddenDmlCheck check = new ForbiddenDmlCheck();
+    List<PlSqlCheck.PreciseIssue> issues = check.scanFileForIssues(context);
+}
     
 }
