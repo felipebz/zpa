464ae487b97de16c3ae2ec04851eb9c0db963367
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2021-05-02 20:47:20 -0300

    Create test for IfStatement tree

---
+++ zpa-core/src/test/kotlin/org/sonar/plsqlopen/sslr/TreeTest.kt
@@ -0,0 +1,38 @@
+/**
+ * Z PL/SQL Analyzer
+ * Copyright (C) 2015-2021 Felipe Zorzo
+ * mailto:felipebzorzo AT gmail DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.plsqlopen.sslr
+
+import org.junit.Before
+import org.sonar.plsqlopen.getSemanticNode
+import org.sonar.plugins.plsqlopen.api.PlSqlGrammar
+import org.sonar.plugins.plsqlopen.api.RuleTest
+
+abstract class TreeTest<T : Tree>(private val root: PlSqlGrammar) : RuleTest() {
+
+    @Before
+    fun init() {
+        setRootRule(root)
+    }
+
+    fun parse(content: String): T {
+        return getSemanticNode(p.parse(content)).tree as T
+    }
+
+}
