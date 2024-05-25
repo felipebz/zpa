77425990928f4dcb1a3854b24e04ddf07c101b0b
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2024-05-24 21:00:55 -0300

    style: Prefer raw strings for regexes in PlSqlLexer

--- zpa-core/src/main/kotlin/org/sonar/plsqlopen/lexer/PlSqlLexer.kt
+++ zpa-core/src/main/kotlin/org/sonar/plsqlopen/lexer/PlSqlLexer.kt
@@ -36,27 +36,28 @@ import org.sonar.plugins.plsqlopen.api.PlSqlTokenType
 
 object PlSqlLexer {
     private val NUMBER_LITERAL = "(?is)(?:" + or(
-        "(?:(?:\\d++(?![.][.])[.]\\d*+)|(?![.][.])[.]\\d++)(?:e[+-]?\\d++)?[fd]?", // decimal value in floating-point literal
-        "\\d++(?:e[+-]?\\d++)?[fd]", // integer value in floating-point literal
-        "\\d++(?:e[+-]?\\d++)") + ")" // number literal in scientific notation
+        """(?:(?:\d++(?![.][.])[.]\d*+)|(?![.][.])[.]\d++)(?:e[+-]?\d++)?[fd]?""", // decimal value in floating-point literal
+        """\d++(?:e[+-]?\d++)?[fd]""", // integer value in floating-point literal
+        """\d++(?:e[+-]?\d++)""" // number literal in scientific notation
+    ) + ")"
 
-    private const val CUSTOM_DELIMITER_START = "[^\\s{\\[<\\(]" // any except spacing
-    private const val CUSTOM_DELIMITER_END = "\\1" // same as the start
+    private const val CUSTOM_DELIMITER_START = """[^\s{\[<\(]""" // any except spacing
+    private const val CUSTOM_DELIMITER_END = """\1""" // same as the start
 
     private val STRING_LITERAL = ("(?is)(?:"
-        + or("?:n?'(?:[^']|'')*+'", // simple text literal
+        + or("""?:n?'(?:[^']|'')*+'""", // simple text literal
         "n?q?'" + or("?:" + g("?:" + g(CUSTOM_DELIMITER_START) + ".*?(?:" + CUSTOM_DELIMITER_END + "')"),
-            g("?:\\(.*?\\)'"),
-            g("?:\\[.*?\\]'"),
-            g("?:<.*?>'"),
-            g("?:\\{.*?\\}'"))) // text with user-defined delimiter
+            g("""?:\(.*?\)'"""),
+            g("""?:\[.*?\]'"""),
+            g("""?:<.*?>'"""),
+            g("""?:\{.*?\}'"""))) // text with user-defined delimiter
         + ")")
 
-    private const val DATE_LITERAL = "(?i)(?:DATE\\s*?'\\d{4}-\\d{2}-\\d{2}')"
+    private const val DATE_LITERAL = """(?i)(?:DATE\s*?'\d{4}-\d{2}-\d{2}')"""
 
-    private val SIMPLE_IDENTIFIER = and("[\\w\\p{L}]", o2n("[\\w\\p{L}#$]"))
+    private val SIMPLE_IDENTIFIER = and("""[\w\p{L}]""", o2n("""[\w\p{L}#$]"""))
 
-    private const val QUOTED_IDENTIFIER = "\".+?\""
+    private const val QUOTED_IDENTIFIER = """".+?""""
 
     fun create(conf: PlSqlConfiguration): Lexer =
         Lexer.builder()
