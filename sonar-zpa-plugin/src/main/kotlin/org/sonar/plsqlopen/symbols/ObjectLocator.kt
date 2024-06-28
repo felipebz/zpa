2334bb8b61ff7668fca06aa4ebee9cb5c8bad860
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2024-06-27 21:12:51 -0300

    fix: Correct coverage import for package bodies declared in same file as package spec

--- sonar-zpa-plugin/src/main/kotlin/org/sonar/plsqlopen/symbols/ObjectLocator.kt
+++ sonar-zpa-plugin/src/main/kotlin/org/sonar/plsqlopen/symbols/ObjectLocator.kt
@@ -22,6 +22,7 @@ package org.sonar.plsqlopen.symbols
 import com.felipebz.flr.api.AstNodeType
 import org.sonar.api.scanner.ScannerSide
 import org.sonar.plsqlopen.squid.SonarQubePlSqlFile
+import org.sonar.plugins.plsqlopen.api.PlSqlGrammar
 import org.sonar.plugins.plsqlopen.api.symbols.Scope
 
 @ScannerSide
@@ -29,14 +30,26 @@ class ObjectLocator {
     private var scope: Scope = ScopeImpl()
 
     private val mappedObjects
-        get() = scope.innerScopes.map {
-            val plSqlFile = it.plSqlFile as SonarQubePlSqlFile? ?: return@map null
-            val identifier = it.identifier ?: return@map null
-            val type = it.type ?: return@map null
-            val firstLine = it.firstToken?.line ?: return@map null
-            val lastLine = it.lastToken?.line ?: return@map null
-            MappedObject(identifier, type, plSqlFile.type(), plSqlFile.path(), plSqlFile.inputFile, firstLine, lastLine)
-        }.filterNotNull()
+        get() = scope.innerScopes
+            .union(scope.innerScopes
+                .flatMap { it.innerScopes }
+                .filter { it.type == PlSqlGrammar.CREATE_PACKAGE_BODY || it.type == PlSqlGrammar.CREATE_TYPE_BODY })
+            .map {
+                val plSqlFile = it.plSqlFile as SonarQubePlSqlFile? ?: return@map null
+                val identifier = it.identifier ?: return@map null
+                val type = it.type ?: return@map null
+                val firstLine = it.firstToken?.line ?: return@map null
+                val lastLine = it.lastToken?.line ?: return@map null
+                MappedObject(
+                    identifier,
+                    type,
+                    plSqlFile.type(),
+                    plSqlFile.path(),
+                    plSqlFile.inputFile,
+                    firstLine,
+                    lastLine
+                )
+            }.filterNotNull()
 
     fun setScope(scope: Scope) {
         this.scope = scope
