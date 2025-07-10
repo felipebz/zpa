93cb217ad7c44cc8537a8b63ce2607b9ae13ebf7
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2025-07-09 21:28:48 -0300

    build: Bump project version

--- plsql-custom-rules/build.gradle.kts
+++ plsql-custom-rules/build.gradle.kts
@@ -23,7 +23,7 @@ java {
 val minSonarQubeVersion = "25.2.0.102705"
 val minSonarQubeApiVersion = "11.1.0.2693"
 val junitVersion = "5.12.2"
-val zpaVersion = "3.8.0"
+val zpaVersion = "3.9.0-SNAPSHOT"
 
 dependencies {
     compileOnly("org.sonarsource.api.plugin:sonar-plugin-api:$minSonarQubeApiVersion")
